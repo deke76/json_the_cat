495fc632f5e114e997e7a97e1dba9c8faa4bc1cf
Darcy <darcy.lauder@shaw.ca>
2022-01-20 10:00:21 -0700

    first commit

---
+++ breedFetcher.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const strURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+let strBreed = process.argv[2];
+
+// console.log(strURL + strBreed);
+
+request((strURL + strBreed), (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  
+  const objData = JSON.parse(body)[0];
+  if (objData) console.log(objData.description);
+  else console.log('Breed not found.')
+  // console.log('error:', error);
+  // console.log('response:', response);
+});
